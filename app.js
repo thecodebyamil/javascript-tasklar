d8e5a59ac6b6ee05a047d5a744c55a99f6d7fdf6
m1mmdli56 <amilmmmdli56@gmail.com>
2026-09-14 22:12:58 +0400

    js added

---
+++ app.js
@@ -0,0 +1,105 @@
+// let hefte = +prompt("gunleri yaz");
+
+// switch (hefte) {
+//   case 1:
+//     console.log("1 ci gundu");
+//     break;
+//   case 2:
+//     console.log("2 ci gundu");
+//     break;
+//   case 3:
+//     console.log("3 ci gundu");
+//     break;
+//   case 4:
+//     console.log("4 ci gundu");
+//     break;
+//   case 5:
+//     console.log("5 ci gundu");
+//     break;
+//   case 6:
+//     console.log("6 ci gundu");
+//     break;
+//   case 7:
+//     console.log("7 ci gundu");
+//     break;
+//   default:
+//     console.log("1-7 arasi reqem yaz");
+// }
+
+//===================================================
+
+// let a = Number(prompt("ededi daxil et"));
+
+// for (let i = a; i > 0; i--) {
+//   console.log(i);
+// }
+
+// let i = 1;
+
+// while (i > 10) {
+//   console.log(i);
+//   i++;
+// }
+
+// let isBool = true;
+// while (isBool) {
+//   let a = prompt("Mehsulun qiymetin yazin yazin");
+
+//   if (a === "stop") {
+//     isBool = false;
+//     break;
+//   }
+//   console.log(a);
+// }
+
+// let a = Number(prompt("ededi daxil et"));
+
+// for (let i = a; i > 0; i--) {
+//   console.log("*");
+// }
+
+// let eded = prompt("Ededi daxil et");
+
+// let a = 1;
+// let b = 1;
+// let c;
+
+// let i = 3;
+
+// while (i <= eded) {
+//   c = a + b;
+//   a = b;
+//   b = c;
+//   i++;
+// }
+
+// console.log(b);
+
+// for (i = 0; i < 100; i++) {
+//   if (i % 2 == 1) {
+//     console.log(i);
+//   }
+// }
+
+//================================================================
+//home work
+
+// let pin = +prompt("Pini daxil et");
+
+// switch (pin) {
+//   case 1234:
+//     console.log("Giris ugurludur!");
+//     break;
+//   default:
+//     console.log("Yanlis PIN!");
+// }
+
+//----------------------------------------------------------------
+
+// let gun = prompt("Oxudugun gunu yaz");
+// let sehife = 300;
+// while (sehife > 0) {
+//   sehife = sehife - gun;
+//   gun++;
+// }
+// console.log(gun);
